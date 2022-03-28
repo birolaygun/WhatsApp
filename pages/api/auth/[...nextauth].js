c3027ab146b77b7a146d2187fd39d13aefcf4c08
birol  aygun <birolaygun.bilisim@gmail.com>
2022-03-28 03:25:37 +0300

    auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,6 +7,7 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      authorizationUrl: process.env.NEXTAUTH_URL,
     }),
     // ...add more providers here
   ],
