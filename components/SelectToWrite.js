c1f93e76111d78eec43a137f8fe0303a129c867e
birol  aygun <birolaygun.bilisim@gmail.com>
2022-03-28 18:17:14 +0300

    unread message try

--- components/SelectToWrite.js
+++ components/SelectToWrite.js
@@ -53,6 +53,8 @@ const SelectToWrite = (props) => {
       Object.entries(data.dbUsers).length === data.dbUsersCount &&
       Object.entries(data.dbConnections).length === data.dbConnectionCount
     ) {
+      console.log("connect");
+
       db.collection("data")
         .doc("SNA9FltXA8h6x6xlt1Ml")
         .update({
@@ -65,6 +67,7 @@ const SelectToWrite = (props) => {
                 .map((side) => side.user)
                 .includes(data.user.userMail)
             ) {
+              console.log(connect);
               return {
                 ...connect,
                 messages: connect.messages.map((message) => {
