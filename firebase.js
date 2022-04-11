48b8bec54f5f4b673adf387932a52366e7860b53
birol  aygun <birolaygun.bilisim@gmail.com>
2022-04-11 03:15:30 +0300

    redleshed

--- firebase.js
+++ firebase.js
@@ -1,3 +1,5 @@
+
+
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,32 +1,3 @@
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-
-// export default NextAuth({
-//   // Configure one or more authentication providers
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//       authorizationUrl: process.env.NEXTAUTH_URL,
-//     }),
-//     // ...add more providers here
-//   ],
-//   pages: {
-//     signIn: "/auth/signin",
-//   },
-//   callbacks: {
-//     async session({ session, token, user }) {
-//       session.user.username = session.user.name
-//         .split(" ")
-//         .join("")
-//         .toLocaleLowerCase();
-
-//       session.user.uid = token.sub;
-
-//       return session;
-//     },
-//   },
-// });
 
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
